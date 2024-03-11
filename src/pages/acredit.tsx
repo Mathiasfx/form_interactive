59b41973302aaa45977d9d52085c9c45fc2d28b2
Mathiasfx <mathias_fx@yahoo.com.ar>
2024-03-11 16:43:02 -0300

    Adding pixie img

--- src/pages/acredit.tsx
+++ src/pages/acredit.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import acreditForm from "../api/acreditForm";
+import Pixie from "../assets/pixie.png";
 
 type FormValues = {
   email: string;
@@ -25,42 +26,51 @@ const Acredit = () => {
 
   useEffect(() => {
     const storedEmail = localStorage.getItem(localStorageKey);
+
     if (storedEmail) {
       setLocalEmail(storedEmail);
     }
   }, []);
 
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
-    console.log(localEmail);
     setIsLoading(true);
     try {
-      await acreditForm(data);
+      const response = await acreditForm(data);
+
+      localStorage.setItem("name", response.user.fullname);
       navigate("/welcome");
     } catch (err: any) {
       console.error(err.response.data.error);
+      if (err.response.status === 404) {
+        navigate("/register");
+      }
       toast.error(err.response.data.error.toString());
     } finally {
       setIsLoading(false);
     }
   };
   return (
-    <div className="bg-black min-h-screen flex flex-col justify-between items-center ">
+    <div className="bg-black min-h-screen flex flex-col justify-between items-center relative">
       <DotBackground>
         <div className="flex w-80 sm:w-full md:w-80 lg:w-1/2 flex-col items-center">
+          <div className="absolute top-20 left-0 right-0 flex justify-center items-center h-50">
+            <img src={Pixie} alt="Pixie" className="w-40" />
+          </div>
           {isLoading ? (
             <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
               <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-100"></div>
             </div>
           ) : (
             <form
-              className="flex flex-col justify-center items-center w-80 "
+              className="flex flex-col justify-center items-center w-80 mt-2"
               onSubmit={handleSubmit(onSubmit)}
             >
               <div className="flex flex-col w-full ">
                 <input
                   className="m-4  bg-black text-white text-lg p-2 rounded border border-gray-300 border-opacity-35"
                   {...register("email", { required: true })}
-                  defaultValue={localEmail}
+                  defaultValue={localEmail ? localEmail : ""}
+                  placeholder="ejemplo@gmail.com"
                 />
                 <label className="text-white text-left pl-5">
                   {errors.email && (
--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
 import { DotBackground } from "../components/ui/dotBackground";
 import Logo from "../assets/logopi.svg";
 
 const Welcome = () => {
+  const [nameStored, setNameStored] = useState<string>("");
+  useEffect(() => {
+    const storedName = localStorage.getItem("name");
+    if (storedName !== null) {
+      setNameStored(storedName);
+    }
+    console.log(storedName);
+  }, []);
   return (
     <div className="bg-black min-h-screen flex justify-center items-center flex-col relative ">
       <DotBackground>
         <p className="text-4xl dark:text-white text-warmGray-200 sm:text-5xl font-bold relative z-20  py-8 text-center">
-          Bienvenido a
+          Bienvenido {nameStored} a{" "}
           <span className="text-yellow-500 dark:text-yellow-500 font-opensans-extrabold">
             Datazón
           </span>
